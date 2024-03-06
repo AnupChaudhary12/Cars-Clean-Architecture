4dbec1e2f1ac8b003caefbe8c57947120889e5ea
Anup Chaudhary <57917543+AnupChaudhary12@users.noreply.github.com>
2024-03-07 04:06:25 +0545

    Created CarNotfoundException and Refactored base entity

---
+++ Cars.Domain/Exceptions/CarNotFoundException.cs
@@ -0,0 +1,17 @@
+﻿using Cars.Shared.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cars.Domain.Exceptions
+{
+    public class CarNotFoundException:SharedExceptions
+    {
+        public CarNotFoundException():base("Car Not Found")
+        {
+            
+        }
+    }
+}
--- Cars.Shared/BaseEntity.cs
+++ Cars.Shared/BaseEntity.cs
@@ -8,8 +8,8 @@ namespace Cars.Shared
     public abstract class BaseEntity
     {
         public int Id { get; set; }
-        public DateTime? CreatedTime { get; set; }
-        public DateTime? ModifiedTime { get; set; }
+        //public DateTime? CreatedTime { get; set; }
+        //public DateTime? ModifiedTime { get; set; }
 
     }
 }
