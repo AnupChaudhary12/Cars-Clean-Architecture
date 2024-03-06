390da17f34b0ef8ef43b1e7cf465467a5cf63710
Anup Chaudhary <57917543+AnupChaudhary12@users.noreply.github.com>
2024-03-07 02:36:13 +0545

    Added shared Exceptions

---
+++ Cars.Shared/Exceptions/SharedExceptions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cars.Shared.Exceptions
+{
+    public abstract class SharedExceptions:Exception
+    {
+        protected SharedExceptions(string message ):base(message)
+        {
+            
+        }
+    }
+}
