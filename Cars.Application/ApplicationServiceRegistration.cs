b8949d89cc740aca8f3de1bea18fd22d58fff22f
Anup Chaudhary <57917543+AnupChaudhary12@users.noreply.github.com>
2024-03-07 03:02:27 +0545

    Added ApplicationServiceRegistraion for services to register services in DI container

---
+++ Cars.Application/ApplicationServiceRegistration.cs
@@ -0,0 +1,20 @@
+﻿
+namespace Cars.Application
+{
+    /// <summary>
+    /// This class is used to register the application services in the DI container.
+    /// </summary>
+    public static class ApplicationServiceRegistration
+    {
+        /// <summary>
+        /// IServiceCollection is a collection of service descriptors.
+        /// It is inbuilt in the Microsoft.Extensions.DependencyInjection namespace.
+        /// </summary>
+        public static  IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+            return services;
+        }
+    }
+}
--- Cars.Application/GlobalUsing.cs
+++ Cars.Application/GlobalUsing.cs
@@ -1,6 +1,4 @@
-﻿global using System;
-global using System.Collections.Generic;
-global using System.Linq;
-global using System.Text;
-global using System.Threading.Tasks;
-global using System.Linq.Expressions;
+﻿global using System.Linq.Expressions;
+global using Microsoft.Extensions.DependencyInjection;
+global using MediatR;
+global using System.Reflection;
