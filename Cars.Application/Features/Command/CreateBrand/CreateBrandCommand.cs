75a690663450926c130a11772dafa7b5c8897ca2
Anup Chaudhary <57917543+AnupChaudhary12@users.noreply.github.com>
2024-03-07 06:13:45 +0545

    Create Command and commandHandler for CQRS pattern for brand

---
+++ Cars.Application/Features/Command/CreateBrand/CreateBrandCommand.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cars.Application.Features.Command.CreateBrand
+{
+    public class CreateBrandCommand: IRequest<int>
+    {
+        public BrandCreateDto? BrandCreateDto { get; set; }
+    }
+}
---
+++ Cars.Application/Features/Command/CreateBrand/CreateBrandCommandHandler.cs
@@ -0,0 +1,34 @@
+﻿using Cars.Application.Contracts.Persistence;
+using Cars.Application.Validators.Brands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cars.Application.Features.Command.CreateBrand
+{
+    public class CreateBrandCommandHandler : IRequestHandler<CreateBrandCommand, int>
+    {
+        private readonly IMapper _mapper;
+        private readonly IUnitOfWork _unitOfWork;
+        public CreateBrandCommandHandler(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;   
+            _mapper = mapper;
+        }
+        public async Task<int> Handle(CreateBrandCommand request, CancellationToken cancellationToken)
+        {
+            var validator = new BrandCreateDtoValidator(_unitOfWork);
+            var validatorResult = await validator.ValidateAsync(request.BrandCreateDto);
+            if (!validatorResult.IsValid)
+            {
+                throw new ValidationException("Invalid Brand", (IEnumerable<FluentValidation.Results.ValidationFailure>)validatorResult);
+            }
+            var brandToCreate = _mapper.Map<Brand>(request.BrandCreateDto);
+            await _unitOfWork.Brands.AddAsync(brandToCreate);
+            await _unitOfWork.Save();
+            return brandToCreate.Id;
+        }
+    }
+}
